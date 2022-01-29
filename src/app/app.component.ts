9376f32c266f394336bedab1fefbbbab9c231398
mamjerez <correomam@gmail.com>
2022-01-29 17:20:31 +0100

    buttonRotate & show sideMenu OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // title = 'OCM-Ayto-Front';
+  @ViewChild("myCheckbox") ck: ElementRef | undefined;
   public sidebarIsOpen = true
-
 
   toggleActive() {
     this.sidebarIsOpen = !this.sidebarIsOpen
+    this.ck!.nativeElement.checked = !this.ck!.nativeElement.checked
   }
 
+  // onCheckboxChange(e: any) {
+  //   if (e.target.checked) {
+  //     this.sidebarIsOpen = !this.sidebarIsOpen
+  //   } else {
+  //     this.sidebarIsOpen = !this.sidebarIsOpen
+  //   }
+  // }
 
 }
 
