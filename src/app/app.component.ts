349b6182552ac1a33001750ddad297cc055ddda0
mamjerez <correomam@gmail.com>
2022-01-29 15:54:56 +0100

    buttonRotate OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'OCM-Ayto-Front';
+  // title = 'OCM-Ayto-Front';
+  public sidebarIsOpen = true
+
+
+  toggleActive() {
+    this.sidebarIsOpen = !this.sidebarIsOpen
+  }
+
+
 }
+
