b1f1de9b8b8529ea71fa94c5d3456125e815f752
mamjerez <correomam@gmail.com>
2022-01-30 12:29:05 +0100

    Add dashboard-ingresos

---
+++ src/app/pages/dashboard-ingresos/dashboard-ingresos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-ingresos',
+  templateUrl: './dashboard-ingresos.component.html',
+  styleUrls: ['./dashboard-ingresos.component.scss']
+})
+export class DashboardIngresosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
