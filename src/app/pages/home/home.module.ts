a0a92359aa078798c74e7216e8cc5638dfcacc1a
mamjerez <correomam@gmail.com>
2022-02-04 18:46:31 +0100

    Deleta DashboardGastosModule

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,11 +3,8 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { DashboardGastosModule } from './dashboard-gastos/dashboard-gastos.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-// import { DashboardGastosComponent } from './dashboard-gastos/dashboard-gastos.component';
-
 
 @NgModule({
   declarations: [
@@ -15,13 +12,10 @@ import { HomeComponent } from './home.component';
     HeaderComponent,
     MenuComponent,
     FooterComponent,
-    // DashboardGastosComponent,
-
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    DashboardGastosModule,
   ]
 })
 export class HomeModule { }
