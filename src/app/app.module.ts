b5bbcf5451d98ad88a60f3b60c63f43a6be80003
mamjerez <correomam@gmail.com>
2022-02-04 10:31:02 +0100

    Add graph-ingresos1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardDataComponent } from './atoms/card/card-data/card-data.component';
-import { CardEvolutionComponent } from './atoms/card/card-evolution/card-evolution.component';
-import { CardGraphComponent } from './atoms/card/card-graph/card-graph.component';
-import { CardIconComponent } from './atoms/card/card-icon/card-icon.component';
-import { CardNameComponent } from './atoms/card/card-name/card-name.component';
-import { CardSimpleComponent } from './components-organisms/cards/card-simple/card-simple.component';
-import { CardComponent } from './molecules/card/card.component';
+import { CardComponent } from './commons/components/card/card.component';
+import { CardDataComponent } from './commons/components/card/components/card-data/card-data.component';
+import { CardEvolutionComponent } from './commons/components/card/components/card-evolution/card-evolution.component';
+import { CardGraphComponent } from './commons/components/card/components/card-graph/card-graph.component';
+import { CardIconComponent } from './commons/components/card/components/card-icon/card-icon.component';
+import { CardNameComponent } from './commons/components/card/components/card-name/card-name.component';
+import { CardSimpleComponent } from './pages/home/dashboard-ingresos-page/components/card-simple/card-simple.component';
+import { GraphIngresos1Component } from './pages/home/dashboard-ingresos-page/components/graph-ingresos1/graph-ingresos1.component';
 import { DashboardIngresosComponent } from './pages/home/dashboard-ingresos-page/dashboard-ingresos.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardIngresosComponent, CardSimpleComponent, CardComponent, CardNameComponent, CardDataComponent, CardEvolutionComponent, CardGraphComponent, CardIconComponent],
+  declarations: [AppComponent, DashboardIngresosComponent, CardSimpleComponent, CardComponent, CardNameComponent, CardDataComponent, CardEvolutionComponent, CardGraphComponent, CardIconComponent, GraphIngresos1Component],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/home/dashboard-ingresos-page/components/graph-ingresos1/graph-ingresos1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'graph-ingresos1',
+  templateUrl: './graph-ingresos1.component.html',
+  styleUrls: ['./graph-ingresos1.component.scss']
+})
+export class GraphIngresos1Component {
+
+}
