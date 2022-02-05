421e88e1bf60e2f20c5e9b3cdabeceac62b95215
mamjerez <correomam@gmail.com>
2022-02-05 10:07:06 +0100

    Install ag-grid

--- src/app/pages/home/programas/programas.module.ts
+++ src/app/pages/home/programas/programas.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgGridModule } from 'ag-grid-angular';
 import { ProgramasComponent } from './programas.component';
 
 export const routes: Routes = [{ path: '', component: ProgramasComponent }]
@@ -12,6 +13,7 @@ export const routes: Routes = [{ path: '', component: ProgramasComponent }]
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    AgGridModule.withComponents([])
   ]
 })
 export class ProgramasModule { }
